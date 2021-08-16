ffe4528a1774cf0c0d8431af0ff33368a93a8b24
jimena <jimenahernando@hotmail.com>
2021-08-16 21:04:47 +0200

    Componente userProfile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,17 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'PrimeraApp';
+export class AppComponent { 
+  
+  arrContacts : any;
+
+  constructor() {
+    this.arrContacts = new Array(
+      { nombre: "Graciela Barreira", telefono: "156560081", email: "gracielabarreira@hotmail.com"},
+      { nombre: "Luis Riera", telefono: "4520593", email:"luisrierawalter@hotmail.com"},
+      { nombre: "Cecilia Hernando", telefono: "153202323", email: "hernandocecilia@live.com"},
+      { nombre: "Sebastian croce", telefono: "675482212", email: "sebacroce93@gmail.com"},
+      { nombre: "Jimena Hernando", telefono: "632439158", email: "jimenahernando@hotmail.com"}
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SaludoComponent } from './components/saludo/saludo.component';
 import { RestarComponent } from './components/restar/restar.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
+import { CronometroComponent } from './components/cronometro/cronometro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SaludoComponent,
-    RestarComponent
+    RestarComponent,
+    UserProfileComponent,
+    CronometroComponent
   ],
   imports: [
     BrowserModule
