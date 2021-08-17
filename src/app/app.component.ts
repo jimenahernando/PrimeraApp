7384b950a0fa99ea61368aa4b78fe65a808085d4
jimena <jimenahernando@hotmail.com>
2021-08-17 11:47:25 +0200

    Componente cronometro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { newArray } from '@angular/compiler/src/util';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,15 +8,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent { 
   
-  arrContacts : any;
+  // arrContacts : any;
+  arrTopes: any;
 
   constructor() {
-    this.arrContacts = new Array(
-      { nombre: "Graciela Barreira", telefono: "156560081", email: "gracielabarreira@hotmail.com"},
-      { nombre: "Luis Riera", telefono: "4520593", email:"luisrierawalter@hotmail.com"},
-      { nombre: "Cecilia Hernando", telefono: "153202323", email: "hernandocecilia@live.com"},
-      { nombre: "Sebastian croce", telefono: "675482212", email: "sebacroce93@gmail.com"},
-      { nombre: "Jimena Hernando", telefono: "632439158", email: "jimenahernando@hotmail.com"}
-    );
+    // this.arrContacts = new Array(
+    //   { nombre: "Graciela Barreira", telefono: "156560081", email: "gracielabarreira@hotmail.com"},
+    //   { nombre: "Luis Riera", telefono: "4520593", email:"luisrierawalter@hotmail.com"},
+    //   { nombre: "Cecilia Hernando", telefono: "153202323", email: "hernandocecilia@live.com"},
+    //   { nombre: "Sebastian croce", telefono: "675482212", email: "sebacroce93@gmail.com"},
+    //   { nombre: "Jimena Hernando", telefono: "632439158", email: "jimenahernando@hotmail.com"}
+    // );
+    
+    // para el cronometro
+    this.arrTopes = [
+      {tope: 9},
+      {tope: 3},
+      {tope: 7},
+    ];
+  }
+
+  onTerminaCrono($event: any){
+    console.log($event);
   }
 }
--- src/app/components/cronometro/cronometro.component.ts
+++ src/app/components/cronometro/cronometro.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-cronometro',
+  selector: 'cronometro',
   templateUrl: './cronometro.component.html',
   styleUrls: ['./cronometro.component.css']
 })
 export class CronometroComponent implements OnInit {
 
-  constructor() { }
+  // @Input() contador: number;
+  @Input('inicio') contador: number;
+
+  //1 - definicion del output
+  @Output() terminaCrono: EventEmitter<string>
+
+  estado: boolean;
+  textoBoton: String;
+
+  constructor() { 
+    this.contador = 10;
+    this.estado = false;
+    this.textoBoton = 'INICIAR';
+    
+    //2- Inicializamos el emmiter
+    this.terminaCrono = new EventEmitter();
+  }
 
   ngOnInit(): void {
   }
 
+  iniciarContador(event: any): void{
+    // cambio el valor por el contrario
+    this.estado = !this.estado;
+    this.textoBoton = this.estado === false ? 'INICIAR' : 'PARAR';
+    const intervalo = setInterval(() => {
+      if (this.estado === false){
+        clearInterval(intervalo);
+        return;
+      }
+      this.contador--;  
+      if(this.contador === 0) {
+          clearInterval(intervalo);
+          //3 - emito la informacion al padre
+          this.terminaCrono.emit(`El cronometro ha terminado`);
+      }
+    } , 1000);
+  }
+
 }
+
+/* FUNCIONES INTERVALICAS: func que nos permiten crear laspsos de tiempo de forma autonoma
+* setInterval(callback, time) -> se repite cada x milisegundos
+* setTimeOut(callback, time) -> solo espera x milisegundas y acciona una sola vez el callback
+*/
